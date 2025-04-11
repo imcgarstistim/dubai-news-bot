af15af7d1215490215f099fc761f3e34070df6f4
imcgarstistim <imcgarstistim@gmail.com>
2025-04-11 14:13:56 +0400

    set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,9 +1,8 @@
 import requests
 
 TOKEN = "7554657413:AAFcXvPt8y4SCX8Q1u8R62aAX-GZmYpseZI"
-URL = f"https://dubai-news-bot-7.onrender.com"
+URL = "https://dubai-news-bot-7.onrender.com"  # آدرس اپت توی Render
 
 set_webhook_url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={URL}"
-
-response = requests.get(set_webhook_url)
-print(response.text)
+res = requests.get(set_webhook_url)
+print("Set webhook response:", res.text)
